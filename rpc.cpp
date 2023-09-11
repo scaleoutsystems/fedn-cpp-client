86f4c2648cce951779b420bd4f455ed97d1b7aeb
Fredrik Wrede <fredrik@scaleoutsystems.com>
2023-09-11 15:45:10 +0200

    SK-538 ModelUpdateRequestStream

--- rpc.cpp
+++ rpc.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
@@ -16,12 +17,18 @@ ABSL_FLAG(std::string, target, "localhost:12080", "Server address");
 using grpc::ChannelInterface;
 using grpc::ClientContext;
 using grpc::Status;
+using grpc::ClientReader;
 using fedn::Connector;
 using fedn::Combiner;
 using fedn::ModelService;
 using fedn::Heartbeat;
+using fedn::ModelUpdate;
 using fedn::ModelUpdateRequest;
+using fedn::ModelResponse;
+using fedn::ModelRequest;
+using fedn::ModelStatus;
 using fedn::Client;
+using fedn::ClientAvailableMessage;
 using fedn::WORKER;
 using fedn::Response;
 
@@ -68,6 +75,86 @@ class GreeterClient {
     }
   }
 
+  void ConnectModelUpdateStream() {
+    // Data we are sending to the server.
+    Client client;
+    client.set_name("test");
+    client.set_role(WORKER);
+
+    ClientAvailableMessage request;
+
+    request.set_allocated_sender(&client);
+
+    ClientContext context;
+
+    // Get ClientReader from stream
+    std::unique_ptr<ClientReader<ModelUpdateRequest> > reader(
+        combinerStub_->ModelUpdateRequestStream(&context, request));
+
+    // Read from stream
+    ModelUpdateRequest modelUpdate;
+    while (reader->Read(&modelUpdate)) {
+      std::cout << "ModelUpdateRequest: " << modelUpdate.model_id() << std::endl;
+      GreeterClient::UpdateLocalModel(modelUpdate.model_id());
+      
+    }
+    reader->Finish();
+    std::cout << "End of stream" << std::endl;
+  }
+  void DownloadModel() {
+
+    // request 
+    ModelRequest request;
+
+    // context
+    ClientContext context;
+
+    // Get ClientReader from stream
+    std::unique_ptr<ClientReader<ModelResponse> > reader(
+        modelserviceStub_->Download(&context, request));
+
+    // Collection for data
+    std::vector<char> accumulatedData;
+
+    // Read from stream
+    ModelResponse modelResponse;
+    while (reader->Read(&modelResponse)) {
+      
+      // Check if status is ModelStatus::IN_PROGRESS
+      if (modelResponse.status() == ModelStatus::IN_PROGRESS) {
+        const std::string& dataResponse = modelResponse.data();
+        accumulatedData.insert(accumulatedData.end(), dataResponse.begin(), dataResponse.end());
+        std::cout << "Download in progress: " << modelResponse.id() << std::endl;
+        std::cout << "Downloaded size: " << accumulatedData.size() << " bytes" << std::endl;
+
+      } else if (modelResponse.status() == ModelStatus::OK) {
+        // Print download complete
+        std::cout << "Download complete" << std::endl;
+      
+      }
+
+    }
+    reader->Finish();
+    std::cout << "Downloaded size: " << accumulatedData.size() << " bytes" << std::endl;
+    std::cout << "Downloaded model: " << modelResponse.id() << std::endl;
+    
+    // Garbage collect the vector
+    accumulatedData.clear();
+    
+    }
+
+  void SendModelUpdateResponse(ModelUpdate modelUpdate) {
+    
+  }
+
+  void UpdateLocalModel(std::string modelID) {
+    std::cout << "Updating local model: " << modelID << std::endl;
+    // Download model from server
+    GreeterClient::DownloadModel();
+
+    
+  }
+
  private:
   std::unique_ptr<Connector::Stub> connectorStub_;
   std::unique_ptr<Combiner::Stub> combinerStub_;
@@ -85,5 +172,6 @@ int main(int argc, char** argv) {
   GreeterClient greeter(
       grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
   greeter.SayHello();
+  greeter.ConnectModelUpdateStream();
   return 0;
 }
